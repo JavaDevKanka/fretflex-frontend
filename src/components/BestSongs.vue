979414c47b7a4d55606371d9ddba734c5bad81f5
JavaDevKanka <daniilsahnov@mail.ru>
2024-06-01 22:49:41 +0300

    Доработки хедера

--- src/components/BestSongs.vue
+++ src/components/BestSongs.vue
@@ -1,17 +1,27 @@
 <template>
   <div>
     <p>ЛУЧШЕЕ</p>
-    <div class="Posts">
-      <div v-if="posts.length > 0">
-        <div v-for="(post, index) in posts" :key="index" class="Post">
-          <div class="SharpNumber"><i>#{{index+1}}</i></div>
-          <img :src="post.songPhoto" :alt="'Изображение песни не загружено!'" />
-          <div class="SongName">{{ post.songName }}</div>
-          <div class="Author">{{ post.authorPseudonym }}</div>
-          <div class="Genre">{{ post.genre }}</div>
+    <div class="PostsWrapper">
+      <div class="Posts">
+        <div v-if="posts.length > 0" class="split-container">
+          <div class="best-post Post">
+            <div class="SharpNumber"><i>#1</i></div>
+            <img :src="posts[0].songPhoto" :alt="'Изображение песни не загружено!'" />
+            <div class="SongName">{{ posts[0].songName }}</div>
+            <div class="Author">{{ posts[0].authorPseudonym }}</div>
+            <div class="Genre">{{ posts[0].genre }}</div>
+          </div>
+          <div class="other-posts">
+            <div v-for="(post, index) in posts.slice(1)" :key="index" :style="postStyle" class="post Post">
+              <div class="SharpNumber"><i>#{{ index + 2 }}</i></div>
+              <div class="SongName">{{ post.songName }}</div>
+              <div class="Author">{{ post.authorPseudonym }}</div>
+              <div class="Genre">{{ post.genre }}</div>
+            </div>
+          </div>
         </div>
+        <p v-else>Загрузка песен...</p>
       </div>
-      <p v-else>Загрузка песен...</p>
     </div>
   </div>
 </template>
@@ -22,45 +32,129 @@ export default {
     return {
       posts: [
         {
-          "id": 1,
-          "songPhoto": "media/images/star_son_image.svg",
-          "songName": "ЗВЕЗДА ПО ИМЕНИ СОЛНЦЕ",
-          "authorPseudonym": "Кино",
-          "genre": "РОК"
+          id: 1,
+          songPhoto: "media/images/star_son_image.svg",
+          songName: "ЗВЕЗДА ПО ИМЕНИ СОЛНЦЕ",
+          authorPseudonym: "Кино",
+          genre: "РОК"
         },
         {
-          "id": 2,
-          "songPhoto": "media/images/ya_tak_soskuchilsya.svg",
-          "songName": "Я ТАК СОСКУЧИЛСЯ",
-          "authorPseudonym": "Порнофильмы",
-          "genre": "РОК"
+          id: 2,
+          songPhoto: "media/images/ya_tak_soskuchilsya.svg",
+          songName: "Я ТАК СОСКУЧИЛСЯ",
+          authorPseudonym: "Порнофильмы",
+          genre: "РОК"
         },
         {
-          "id": 3,
-          "songPhoto": "media/images/batareyka.svg",
-          "songName": "БАТАРЕЙКА",
-          "authorPseudonym": "Жуки","genre": "РОК"
+          id: 3,
+          songPhoto: "media/images/batareyka.svg",
+          songName: "БАТАРЕЙКА",
+          authorPseudonym: "Жуки",
+          genre: "РОК"
         },
         {
-          "id": 4,
-          "songPhoto": "media/images/lesnik.svg",
-          "songName": "ЛЕСНИК",
-          "authorPseudonym": "Король и Шут",
-          "genre": "РОК"
+          id: 4,
+          songPhoto: "media/images/lesnik.svg",
+          songName: "ЛЕСНИК",
+          authorPseudonym: "Король и Шут",
+          genre: "РОК"
         },
         {
-          "id": 5,
-          "songPhoto": "media/images/poteryanniy_ray.svg",
-          "songName": "ПОТЕРЯННЫЙ РАЙ",
-          "authorPseudonym": "Ария",
-          "genre": "РОК"
+          id: 5,
+          songPhoto: "media/images/poteryanniy_ray.svg",
+          songName: "ПОТЕРЯННЫЙ РАЙ",
+          authorPseudonym: "Ария",
+          genre: "РОК"
         }
       ]
     };
   },
+
+computed: {
+  postStyle() {
+    return {
+      height: `calc(100% / ${this.posts.length - 1})`
+    };
+  }
+}
 };
+
 </script>
 
 <style scoped>
-/* Ваши стили могут быть применены здесь */
+.split-container {
+  display: flex;
+  width: 100%;
+}
+
+.best-post {
+  width: 50%;
+  border-right: 2px solid black;
+  box-sizing: border-box;
+  padding: 5px;
+  position: relative;
+}
+
+.Post {
+  border: 1px solid rgba(255, 255, 255, 0.5);
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  height: 100%;
+}
+
+.Post img {
+  width: 40%;
+  height: auto;
+  margin: 0 auto;
+}
+
+.SongName {
+  padding-bottom: 1%;
+  padding-left: 2%;
+  align-self: flex-start;
+}
+
+.Author {
+  padding-bottom: 2%;
+  padding-left: 2%;
+  align-self: flex-start;
+}
+
+.Genre {
+  padding-top: 2%;
+  padding-left: 2%;
+  position: absolute;
+  top: 5px;
+  left: 5px;
+}
+
+.SharpNumber {
+  position: absolute;
+  top: 5px;
+  right: 5px;
+  padding-top: 2%;
+  padding-right: 2%;
+}
+
+.other-posts {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+}
+
+.other-posts .post {
+  padding: 5px;
+}
+.Post img {
+  width: 40%;
+  height: auto;
+  object-fit: cover;
+}
+
+.Post .SharpNumber {
+  position: absolute;
+  top: 5px;
+  right: 5px;
+}
 </style>
